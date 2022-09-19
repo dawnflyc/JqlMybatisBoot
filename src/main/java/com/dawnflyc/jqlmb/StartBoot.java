5bd4aafe1d9736fef8d5eef1076e1f68e7910a8b
dawnflyc <dawnflyc@gmail.com>
2022-09-19 19:49:04 +0800

    初始化0.0.1

---
+++ src/main/java/com/dawnflyc/jqlmb/StartBoot.java
@@ -0,0 +1,10 @@
+package com.dawnflyc.jqlmb;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"com.dawnflyc.jqlmb"})
+public class StartBoot {
+}
